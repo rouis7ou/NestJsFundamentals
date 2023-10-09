17ef61031a98b1ed9499e621552e1178a479b276
houssem97 <houssem.rouis.1@ens.etsmtl.ca>
2023-10-09 14:41:45 +0100

    end of nestFundamentals

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventSchema } from 'src/events/entities/event.entity/event.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Coffee.name,
+        schema: CoffeeSchema,
+      },
+      {
+        name: Event.name,
+        schema: EventSchema,
+      },
+    ]),
+  ],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
---
+++ src/events/entities/event.entity/event.entity.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema()
+export class Event extends mongoose.Document {
+  // Note "entity" was removed from the class "name"
+  @Prop()
+  type: string;
+
+  @Prop({ index: true })
+  name: string;
+
+  @Prop({ type: mongoose.SchemaTypes.Mixed })
+  payload: Record<string, any>;
+}
+
+export const EventSchema = SchemaFactory.createForClass(Event);
+
+EventSchema.index({ name: 1, type: -1 });
